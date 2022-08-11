8fe4ffc8ccf01fb38daf6fbe1401f801fcd05bb0
kemeice <kem@kemstech.com>
2022-08-11 15:07:50 -0500

    starting login from scratch

---
+++ app/controllers/confirmations_controller.rb
@@ -0,0 +1,29 @@
+class ConfirmationsController < ApplicationController
+  before_action :redirect_if_authenticated, only: [:create, :new]
+    def create
+      def create
+        @user = User.find_by(email: params[:user][:email].downcase)
+    
+        if @user.present? && @user.unconfirmed?
+          @user.send_confirmation_email!
+          ...
+        end
+      
+      end
+    
+      def edit
+        @user = User.find_signed(params[:confirmation_token], purpose: :confirm_email)
+    
+        if @user.present?
+          @user.confirm!
+          login @user
+          redirect_to root_path, notice: "Your account has been confirmed."
+        else
+          redirect_to new_confirmation_path, alert: "Invalid token."
+        end
+      end
+    
+      def new
+        @user = User.new
+      end
+end
